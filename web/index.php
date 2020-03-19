1233b3f8e00cf3131f4c9d9c1b048a9f7c9782f7
akrez <akrez.like@gmail.com>
2020-03-19 22:59:48 +0330

    dg

--- web/index.php
+++ web/index.php
@@ -164,4 +164,13 @@ if (YII_ENV == 'dev') {
     ];
 }
 
-(new yii\web\Application($config))->run();
+$application = new yii\web\Application($config);
+$application->on(yii\web\Application::EVENT_BEFORE_REQUEST, function(yii\base\Event $event) {
+    $event->sender->response->on(yii\web\Response::EVENT_BEFORE_SEND, function($e) {
+        ob_start("ob_gzhandler");
+    });
+    $event->sender->response->on(yii\web\Response::EVENT_AFTER_SEND, function($e) {
+        ob_end_flush();
+    });
+});
+$application->run();
