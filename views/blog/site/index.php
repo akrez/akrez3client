31abcf32d20e6c9c4719aeaa53b8541849565fd7
akrez <akrez.like@gmail.com>
2020-03-20 16:46:27 +0330

    gfhdgh

--- views/blog/site/index.php
+++ views/blog/site/index.php
@@ -7,7 +7,7 @@ use yii\helpers\HtmlPurifier;
 
 <div class="v1-default-index">
     <h1 class="mt0" style="display: inline-block;"><?= HtmlPurifier::process(Yii::$app->blog->attribute('title')) ?></h1>
-    <h2 class="mt0" style="display: inline-block;margin-right: 10px;"><small><?= Yii::$app->blog->attribute('slug') ?></small></h2>
+    <h2 class="mt0 h2text" style="display: inline-block;margin-right: 10px;"><?= Yii::$app->blog->attribute('slug') ?></h2>
     <h5 class="mt0" style="text-align: justify;line-height: 1.62em;font-size: 14px;"><?= Yii::$app->blog->attribute('des') ?></h5>
 </div>
 
@@ -15,7 +15,6 @@ use yii\helpers\HtmlPurifier;
     <?= Html::beginForm(BlogHelper::url('site/index'), 'GET'); ?>
     <div class="col-sm-5">
         <div class="input-group">
-
             <?= Html::textInput('Search[title][0][value]', (isset(Yii::$app->view->params['search']['title'][0]['value']) ? Yii::$app->view->params['search']['title'][0]['value'] : null), ['class' => 'form-control']); ?>
             <?= Html::hiddenInput('Search[title][0][operation]', 'LIKE'); ?>
             <span class="input-group-btn">
