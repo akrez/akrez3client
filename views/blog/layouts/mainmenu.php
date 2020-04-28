33a8300b42964e339c4ea7e22d10cc9bf3cebbfa
akrez <akrez.like@gmail.com>
2020-04-28 16:28:34 +0430

    dfg

--- views/blog/layouts/mainmenu.php
+++ views/blog/layouts/mainmenu.php
@@ -10,30 +10,61 @@ use yii\web\JsExpression;
 use yii\web\View;
 
 $this->registerJs('
-    $("body").on("click", ".btn-delete", function(){
+    $("body").on("click", ".btn-delete", function () {
         $(this).closest(".filter").find("input[type=text]").val("");
     });
-    
-    function sliderCheckboxOnChange(event) {
-        var checkbox = $(event.target);
-        var idPrefix = checkbox.attr("data-idprefix");
-        if(checkbox.is(":checked")){
-            $(checkbox).parents(".panel").find(".panel-body").slideDown();
-            $(checkbox).removeClass("panel-collapsed");
-            $("#"+idPrefix+"-input-min").prop("disabled", false);
-            $("#"+idPrefix+"-input-max").prop("disabled", false);
-        } else {
-            $(checkbox).parents(".panel").find(".panel-body").slideUp();
-            $(checkbox).addClass("panel-collapsed");
-            $("#"+idPrefix+"-input-min").prop("disabled", true);
-            $("#"+idPrefix+"-input-max").prop("disabled", true);
+    $(document).on("click", ".panel-akrez > .panel-heading", function (e) {
+        var that = $(this);
+        var idPrefix = that.attr("data-idprefix");
+        var checkbox = that.find("input[type=checkbox]").first();
+        if (checkbox.length < 1) {
+            checkbox = null;
         }
-    }
- 
-', View::POS_END);
+        //
+        showPanel = false;
+        if (that.hasClass("panel-collapsed")) {
+            showPanel = true;
+        }
+        //
+        if (showPanel) {
+            that.parents(".panel").find(".panel-body").slideDown();
+            that.removeClass("panel-collapsed");
+        } else {
+            that.parents(".panel").find(".panel-body").slideUp();
+            that.addClass("panel-collapsed");
+        }
+        if (idPrefix) {
+            $("#" + idPrefix + "-input-min").prop("disabled", !showPanel);
+            $("#" + idPrefix + "-input-max").prop("disabled", !showPanel);
+        }
+        if (checkbox) {
+            checkbox.prop("checked", showPanel);
+        }
+    });
+
+', View::POS_READY);
 $this->registerCss('
     .slider {
 	width: calc(100%) !important;
+    }
+    .panel-akrez .panel-heading {
+        padding: 6px 12px;
+        font-weight: 700;
+        color: #555555;
+        background-color: #eeeeee;
+        cursor: pointer;
+        user-select: none;
+        margin-bottom: 0;
+        display: block;
+    }
+    .panel-akrez .panel-body {
+        padding: 6px 12px;
+        max-height: 170px;
+        overflow-y: auto;
+    }
+    .panel-akrez .panel-heading > input[type=checkbox] {
+        margin-left: 12px;
+        display: inline-block;
     }
 ');
 $constant = BlogHelper::getConstant();
@@ -66,25 +97,21 @@ $constant = BlogHelper::getConstant();
     <div class="row">
 
         <?php
-        $this->registerJs('
-            $(document).on("click", ".panel .clickable", function (e) {
-                var that = $(this);
-                if (that.hasClass("panel-collapsed")) {
-                    that.parents(".panel").find(".panel-body").slideDown();
-                    that.removeClass("panel-collapsed");
-                } else {
-                    that.parents(".panel").find(".panel-body").slideUp();
-                    that.addClass("panel-collapsed");
-                }
-            });
-        ', View::POS_READY);
         $i = 0;
         foreach (Yii::$app->view->params['search'] as $fieldId => $fieldFilters) :
             $field = Yii::$app->view->params['fields'][$fieldId];
             $type = $field['type'];
             $widgets = (array) $field['widgets'];
             foreach ($widgets as $widget) :
-                $filter = ['operation' => null, 'value' => null];
+
+                $filter = [
+                    'operation' => null,
+                    'value' => null,
+                    'values' => [],
+                    'value_min' => null,
+                    'value_max' => null,
+                ];
+
                 foreach ((array) $fieldFilters as $fieldFilterKey => $fieldFilter) :
                     if ($fieldFilter['widget'] == $widget) {
                         $filter = Yii::$app->view->params['search'][$fieldId][$fieldFilterKey];
@@ -92,14 +119,17 @@ $constant = BlogHelper::getConstant();
                         continue;
                     }
                 endforeach;
+
                 $namePrefix = 'Search[' . $fieldId . '][' . $i . ']';
                 echo Html::hiddenInput($namePrefix . '[widget]', $widget);
+
                 if ($type == FieldList::TYPE_STRING || $type == FieldList::TYPE_NUMBER):
                     if ($type == FieldList::TYPE_NUMBER) {
                         $specialWidgets = ['>=' => BlogHelper::getConstant('widget', $type, '>='), '<=' => BlogHelper::getConstant('widget', $type, '<='), '=' => BlogHelper::getConstant('widget', $type, '='), '<>' => BlogHelper::getConstant('widget', $type, '<>')];
                     } else {
                         $specialWidgets = ['LIKE' => BlogHelper::getConstant('widget', $type, 'LIKE'), 'NOT LIKE' => BlogHelper::getConstant('widget', $type, 'NOT LIKE'), '=' => BlogHelper::getConstant('widget', $type, '='), '<>' => BlogHelper::getConstant('widget', $type, '<>')];
                     }
+
                     if (in_array($widget, array_keys($specialWidgets))):
                         ?>
                         <div class="col-sm-12 pb20 filter">
@@ -141,35 +171,44 @@ $constant = BlogHelper::getConstant();
                         <?php
                     elseif ($widget == 'SINGLE') :
                         $items = Helper::normalizeArray($field['options'], true);
+                        $disabled = !boolval($filter['value']);
                         ?>
-                        <div class="col-sm-12 pb20 filter">
-                            <div class="panel panel-default">
-                                <div class="panel-heading clickable" style="padding-top: 5px;padding-bottom: 0px;padding-right: 12px;padding-left: 12px;background: #eeeeee; cursor: pointer;">
-                                    <?= Html::tag('label', $field['title'] . '<small>' . $field['unit'] . '</small>', ['class' => 'control-label', 'style' => 'color: #555555;']) ?>
+                        <div class="col-sm-12 filter">
+                            <div class="panel panel-default panel-akrez">
+                                <div class="panel-heading <?= $disabled ? 'panel-collapsed' : '' ?>">
+                                    <?= Html::checkbox(null, !$disabled); ?>
+                                    <?= HtmlPurifier::process($field['title']) ?>
+                                    <?= $field['unit'] ? ' <small>(' . HtmlPurifier::process($field['unit']) . ')</small> ' : '' ?>
                                 </div>
-                                <div class="panel-body" style="padding-right: 12px;padding-left: 12px;padding-top: 6px;padding-bottom: 6px;">
+                                <div class="panel-body" style="<?= $disabled ? 'display: none;' : '' ?>">
                                     <?= Html::hiddenInput($namePrefix . '[operation]', '='); ?>
                                     <?= Html::radioList($namePrefix . '[value]', $filter['value'], array_combine($items, $items), ['separator' => "<br />", 'encode' => false]) ?>
                                     <a onclick="$(this).closest('.filter').find('input[type=radio]').prop('checked', false)" href="javascript:void(0);" style="padding-right: 15px;"><small class="control-label">(هیچکدام)</small></a>
                                 </div>
                             </div>
                         </div>
+
                         <?php
                     elseif ($widget == 'MULTI') :
                         $items = Helper::normalizeArray($field['options'], true);
+                        $disabled = !boolval($filter['values']);
                         ?>
+
                         <div class="col-sm-12 filter">
-                            <div class="panel panel-default">
-                                <div class="panel-heading clickable" style="padding-top: 5px;padding-bottom: 0px;padding-right: 12px;padding-left: 12px;background: #eeeeee; cursor: pointer;">
-                                    <?= Html::tag('label', $field['title'], ['class' => 'control-label', 'style' => 'color: #555555;']) . Html::tag('small', $field['unit']) ?>
+                            <div class="panel panel-default panel-akrez">
+                                <div class="panel-heading <?= $disabled ? 'panel-collapsed' : '' ?>">
+                                    <?= Html::checkbox(null, !$disabled); ?>
+                                    <?= HtmlPurifier::process($field['title']) ?>
+                                    <?= $field['unit'] ? ' <small>(' . HtmlPurifier::process($field['unit']) . ')</small> ' : '' ?>
                                 </div>
-                                <div class="panel-body" style="padding-right: 12px;padding-left: 12px;padding-top: 6px;padding-bottom: 6px;">
+                                <div class="panel-body" style="<?= $disabled ? 'display: none;' : '' ?>">
                                     <?= Html::hiddenInput($namePrefix . '[operation]', 'IN'); ?>
-                                    <?= Html::checkboxList($namePrefix . '[value][]', $filter['value'], array_combine($items, $items), ['separator' => "<br />", 'encode' => false]) ?>
+                                    <?= Html::checkboxList($namePrefix . '[values][]', $filter['values'], array_combine($items, $items), ['separator' => "<br />", 'encode' => false]) ?>
                                     <a onclick="$(this).closest('.filter').find('input[type=checkbox]').prop('checked', false)" href="javascript:void(0);" style="padding-right: 15px;"><small class="control-label">(هیچکدام)</small></a>
                                 </div>
                             </div>
                         </div>
+
                         <?php
                     elseif ($widget == 'BETWEEN'):
                         $idPrefix = 'Search-' . $fieldId . '-' . $i;
@@ -193,36 +232,41 @@ $constant = BlogHelper::getConstant();
                         $min = floatval($min);
                         $max = floatval($max == $min ? $max + 1 : $max);
                         //
-                        if (!is_array($filter['value'])) {
-                            $filter['value'] = Helper::normalizeArray($filter['value'], true);
-                        }
+                        $filter = $filter + ['value_min' => null, 'value_max' => null,];
+                        //
                         $disabled = true;
-                        if (count($filter['value']) == 2) {
+                        if ($filter['value_min'] && $filter['value_max']) {
+                            $disabled = false;
+                        } elseif ($filter['value_min']) {
+                            $filter['value_max'] = $max;
+                            $disabled = false;
+                        } elseif ($filter['value_max']) {
+                            $filter['value_min'] = $min;
                             $disabled = false;
                         } else {
-                            $filter['value'] = [
-                                0 => $min,
-                                1 => $max,
-                            ];
+                            $filter['value_min'] = $min;
+                            $filter['value_max'] = $max;
                         }
                         //
                         echo Html::hiddenInput($namePrefix . '[operation]', $widget);
                         ?>
+
                         <div class="col-sm-12 filter">
-                            <div class="panel panel-default">
-                                <div class="panel-heading <?= $disabled ? 'panel-collapsed' : '' ?>" style="padding-top: 4px;padding-bottom: 0px;padding-right: 12px;padding-left: 12px;background: #eeeeee;">
-                                    <?= Html::checkbox($namePrefix . '[value]', !$disabled, ['id' => $idPrefix . '-checkbox', 'style' => 'padding-top: 6px;padding-bottom: 6px;margin-left: 12px;border-left: black solid 1px;', 'data-idPrefix' => $idPrefix, 'onchange' => 'sliderCheckboxOnChange(event);',]); ?> 
-                                    <?= Html::tag('label', $field['title'], ['for' => $idPrefix . '-checkbox', 'class' => 'control-label', 'style' => 'margin-top: 0px;margin-bottom: 4px; user-select: none; margin-right: 12px;color: #555;width: calc(100% - 40px);']) ?>
+                            <div class="panel panel-default panel-akrez">
+                                <div class="panel-heading <?= $disabled ? 'panel-collapsed' : '' ?>" data-idPrefix="<?= $idPrefix ?>">
+                                    <?= Html::checkbox(null, !$disabled); ?>
+                                    <?= HtmlPurifier::process($field['title']) ?>
+                                    <?= $field['unit'] ? ' <small>(' . HtmlPurifier::process($field['unit']) . ')</small> ' : '' ?>
                                 </div>
-                                <div class="panel-body" style="<?= $disabled ? 'display: none;' : '' ?>padding-right: 12px;padding-left: 12px;padding-top: 6px;padding-bottom: 6px;">
+                                <div class="panel-body" style="<?= $disabled ? 'display: none;' : '' ?>">
                                     <?php
-                                    echo "<div><span style='float: left;' id='$idPrefix-label-min'>" . number_format($filter['value'][0]) . "</span><span style='float: right;' id='$idPrefix-label-max'>" . number_format($filter['value'][1]) . "</span><div class='clearfix'></div></div>";
+                                    echo "<div><span style='float: left;' id='$idPrefix-label-min'>" . number_format($filter['value_min']) . "</span><span style='float: right;' id='$idPrefix-label-max'>" . number_format($filter['value_max']) . "</span><div class='clearfix'></div></div>";
                                     echo Slider::widget([
                                         'options' => [
                                             'id' => $idPrefix . '-slider',
                                         ],
                                         'name' => $namePrefix . "[input]",
-                                        'value' => implode(',', $filter['value']),
+                                        'value' => implode(',', [$filter['value_min'], $filter['value_max']]),
                                         'sliderColor' => Slider::TYPE_GREY,
                                         'pluginOptions' => [
                                             'min' => $min,
@@ -240,27 +284,27 @@ $constant = BlogHelper::getConstant();
                                         ],
                                     ]);
 
-                                    echo Html::hiddenInput($namePrefix . '[value][0]', $filter['value'][0], ['id' => $idPrefix . '-input-min'] + ($disabled ? ['disabled' => 'disabled'] : []));
-                                    echo Html::hiddenInput($namePrefix . '[value][1]', $filter['value'][1], ['id' => $idPrefix . '-input-max'] + ($disabled ? ['disabled' => 'disabled'] : []));
+                                    echo Html::hiddenInput($namePrefix . '[value_min]', $filter['value_min'], ['id' => $idPrefix . '-input-min'] + ($disabled ? ['disabled' => 'disabled'] : []));
+                                    echo Html::hiddenInput($namePrefix . '[value_max]', $filter['value_max'], ['id' => $idPrefix . '-input-max'] + ($disabled ? ['disabled' => 'disabled'] : []));
                                     ?>
                                 </div>
                             </div>
                         </div>
+
                         <?php
                     endif;
                 elseif ($type == FieldList::TYPE_BOOLEAN) :
                     if ($widget == '2STATE') :
                         $idPrefix = 'Search-' . $fieldId . '-' . $i;
                         ?>
-                        <div class="col-sm-12 pb20 filter">
-                            <div class="input-group"> 
-                                <span class="input-group-addon">
-                                    <?= Html::checkbox($namePrefix . '[value]', $filter['value'], ['id' => $idPrefix]); ?> 
-                                </span>
-                                <?= Html::hiddenInput($namePrefix . '[operation]', '='); ?>
-                                <span class="form-control" style="background-color: #eeeeee;padding-right: 12px;padding-left: 12px;">
-                                    <?= Html::tag('label', $field['title'], ['for' => $idPrefix, 'class' => 'control-label', 'style' => 'margin-top: 3px;margin-bottom: 2px;width: 100%; user-select: none; ']) ?>
-                                </span>
+                        <div class="col-sm-12 filter">
+                            <div class="panel panel-default panel-akrez">
+                                <div class="panel-heading <?= $filter['value'] ? '' : 'panel-collapsed' ?>" for="<?= $idPrefix ?>">
+                                    <?= Html::checkbox($namePrefix . '[value]', $filter['value'], ['id' => $idPrefix,]); ?> 
+                                    <?= Html::hiddenInput($namePrefix . '[operation]', '='); ?>
+                                    <?= HtmlPurifier::process($field['title']) ?>
+                                    <?= $field['unit'] ? ' <small>(' . HtmlPurifier::process($field['unit']) . ')</small> ' : '' ?>
+                                </div>
                             </div>
                         </div>
                     <?php elseif ($widget == '3STATE') : ?>
